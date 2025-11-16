aa3d8fd4887d38add7342fa58412f8f061fb63a1
Arsyharif006 <aryaarmdhn006@gmail.com>
2025-11-16 22:33:01 +0700

    first deploy

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    base: '/AIsyrfBolt/', 
+    base: '/Syrion-app', 
     server: {
       host: '0.0.0.0',
       port: 3000,
